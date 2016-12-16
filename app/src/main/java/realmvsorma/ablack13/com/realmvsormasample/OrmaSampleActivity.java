2e343f7f227fa429c60f315fcdf9added23f7862
scijoker@gmail.com <jrose1993>
2016-12-16 10:45:56 +0200

    Add dispose support to list(RxJava2 feature)

--- app/src/main/java/realmvsorma/ablack13/com/realmvsormasample/OrmaSampleActivity.java
+++ app/src/main/java/realmvsorma/ablack13/com/realmvsormasample/OrmaSampleActivity.java
@@ -1,8 +1,6 @@
 package realmvsorma.ablack13.com.realmvsormasample;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -20,6 +18,7 @@ import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import realmvsorma.ablack13.com.realmvsormasample.adapters.SimpleOrmaAdapter;
@@ -27,7 +26,7 @@ import realmvsorma.ablack13.com.realmvsormasample.beans.OrmaBeanObj;
 import realmvsorma.ablack13.com.realmvsormasample.beans.OrmaBeanObj_Relation;
 
 public class OrmaSampleActivity extends AppCompatActivity {
-    private Button btnAdd;
+    private Button btnAdd, btnDispose;
     private SimpleOrmaAdapter adapter;
     private RecyclerView recyclerView;
     private Disposable loadList;
@@ -41,55 +40,62 @@ public class OrmaSampleActivity extends AppCompatActivity {
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         btnAdd = (Button) findViewById(R.id.btn_add);
+        btnDispose = (Button) findViewById(R.id.btn_dispose);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(OrmaSampleActivity.this));
-        fillListData();
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 final OrmaBeanObj obj = new OrmaBeanObj();
-                obj.name = "New orma itdsddsm" + System.currentTimeMillis();
+                obj.name = "New orma " + System.currentTimeMillis();
                 obj.lastName = "sdsdasda";
                 App.orma().relationOfOrmaBeanObj().upserter().execute(obj);
             }
         });
-        fillListData().subscribe(new Consumer<List<OrmaBeanObj>>() {
+        btnDispose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (loadList != null && !loadList.isDisposed()) {
+                    Toast.makeText(OrmaSampleActivity.this, "Load list disposed", Toast.LENGTH_LONG).show();
+                    loadList.dispose();
+                }
+            }
+        });
+        adapter = new SimpleOrmaAdapter();
+        recyclerView.setAdapter(adapter);
+        loadList = fillListData().subscribe(new Consumer<List<OrmaBeanObj>>() {
             @Override
             public void accept(List<OrmaBeanObj> ormaBeanObjs) throws Exception {
                 adapter.setItems(ormaBeanObjs);
             }
         });
-        adapter = new SimpleOrmaAdapter();
-        recyclerView.setAdapter(adapter);
-
-        new Handler(Looper.getMainLooper())
-                .postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        if (loadList != null && !loadList.isDisposed()) {
-                            loadList.dispose();
-                        }
-                    }
-                }, 5000);
     }
 
     private Observable<List<OrmaBeanObj>> fillListData() {
+        final Disposable[] updaterListenerDisposable = {null};
         return Observable.create(new ObservableOnSubscribe<List<OrmaBeanObj>>() {
             @Override
             public void subscribe(final ObservableEmitter<List<OrmaBeanObj>> emitter) throws Exception {
-                final OrmaBeanObj_Relation ormaBeanObjs = App.orma().relationOfOrmaBeanObj();
+                OrmaBeanObj_Relation ormaBeanObjs = App.orma().relationOfOrmaBeanObj();
                 Observable<Selector<OrmaBeanObj, ?>> queryObservable = ormaBeanObjs.createQueryObservable();
-                loadList = queryObservable.subscribeOn(Schedulers.io())
+                updaterListenerDisposable[0] = queryObservable.subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(new Consumer<Selector<OrmaBeanObj, ?>>() {
                             @Override
-                            public void accept(Selector<OrmaBeanObj, ?> selector) throws Exception {
-                                Toast.makeText(OrmaSampleActivity.this, "Db changed!", Toast.LENGTH_LONG).show();
-                              emitter.onNext(selector.toList());
+                            public void accept(Selector<OrmaBeanObj, ?> ormaBeanObjs) throws Exception {
+                                Toast.makeText(OrmaSampleActivity.this, "Db updated", Toast.LENGTH_LONG).show();
+                                emitter.onNext(ormaBeanObjs.toList());
                             }
                         });
                 emitter.onNext(ormaBeanObjs.selector().toList());
             }
-        }).subscribeOn(Schedulers.io());
+        }).doOnDispose(new Action() {
+            @Override
+            public void run() throws Exception {
+                if (updaterListenerDisposable[0] != null && !updaterListenerDisposable[0].isDisposed()) {
+                    updaterListenerDisposable[0].dispose();
+                }
+            }
+        });
     }
 }
